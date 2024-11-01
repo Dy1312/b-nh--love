2d7a0597a31ab50b442bfe1bd4da0e0b757a88eb
Dy1312 <hoangduy130106@gmail.com>
2024-11-01 16:37:35 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -7,4 +7,8 @@ const CONFIGDATA = {
   titleModar: "Cảm ơn nhìuuuuu.",
   descriptionModar:
     "Cảm ơn bé nhí đã cho tui một cơ hội để tui có thể ở bên cạnh, che chở và bảo vệ bé. </br> Tui biết bé nhí có thể tự lo cho bản thân mình nhưng mà tui cũng muốn bé nhớ rằng là tui luôn ở phía sau để có thể giúp bé nhí bất cứ lúc nào đó nha nên đừng ngần ngại mà chia sẻ với tui. </br> tuy rằng mới quen mà đã xa nhau tận 3 tuần dài đằng đẳng (quá lâu) nhưng mà tui vẫn yêu bé nhí ko có thay đổi hay suy nghĩ gì hết à nha-.-, nên đừng có suy nghĩ gì tào lao hết á. </br> lần nữa cảm ơn bé nhí rất nhìu vì đã chịu tui nha và sẽ còn dài mãi về sauu </br> Love you <3",
+<audio controls>
+  <source src="https://zingmp3.vn/album/Hen-Gap-Em-Duoi-Anh-Trang-Single-HURRYKNG-HIEUTHUHAI-MANBO/67CIZWCI.html.mp3" type="audio/mpeg">
+</audio>
 };
+
